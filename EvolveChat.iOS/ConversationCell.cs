9c769f35372f60670ab759a36ee66e89de243909
Alex Corrado <alexc@xamarin.com>
2016-04-12 14:51:25 -0400

    Simplify ConversationCell
    Embrace the beauty of C# 6!

--- EvolveChat.iOS/ConversationCell.cs
+++ EvolveChat.iOS/ConversationCell.cs
@@ -15,52 +15,39 @@ namespace EvolveChat {
 
 		public static readonly NSString Id = new NSString ("ConversationCell");
 
-		List<IDisposable> disposables = new List<IDisposable> ();
+		IDisposable participantSubscription, messageSubscription, timeStampSubscription;
 
 		public Conversation Conversation {
 			get { return conversation; }
 			set {
 				if (conversation != value) {
-					// Clean up if we're being reused
-					foreach (var d in disposables)
-						d.Dispose ();
-					disposables.Clear ();
-
 					// Configure the cell for the new conversation...
 					conversation = value;
 
 					// Set the name(s), and update them if someone joins the conversation
-					disposables.Add (
-						conversation.ObservePropertyValue (c => c.Participants)
-							.ObserveOn (SynchronizationContext.Current)
-							.Subscribe (participants => {
-								// Get the participants in the conversation who are not the current user
-								var others = participants.Where (p => p != App.Backend.Me).ToList ();
-								name.Text = string.Join (", ", others);
-								badge.Text = (others.Count == 1)? (others [0].FirstName.Substring (0, 1) + others [0].LastName.Substring (0, 1)) : others.Count.ToString ();
-							})
-					);
+					participantSubscription?.Dispose ();
+					participantSubscription = conversation.ObservePropertyValue (c => c.Participants)
+						.ObserveOn (SynchronizationContext.Current)
+						.Subscribe (participants => {
+							// Get the participants in the conversation who are not the current user
+							var others = participants.Where (p => p != App.Backend.Me).ToList ();
+							name.Text = string.Join (", ", others);
+							badge.Text = (others.Count == 1)? (others [0].FirstName.Substring (0, 1) + others [0].LastName.Substring (0, 1)) : others.Count.ToString ();
+						});
 
 					// Set the last message preview and date, and update them if a new message is received
-					IDisposable lastTimeStampSubscription = null;
-					disposables.Add (
-						conversation.Messages.ObserveLastAndAddedItems ()
-							.ObserveOn (SynchronizationContext.Current)
-							.Subscribe (msg => {
-								// Simply set the preview label to the entire message; UIKit will take care
-								//  of line breaks and truncation.
-								preview.Text = msg.Text;
+					messageSubscription?.Dispose ();
+					messageSubscription = conversation.Messages.ObserveLastAndAddedItems ()
+						.ObserveOn (SynchronizationContext.Current)
+						.Subscribe (msg => {
+							// Simply set the preview label to the entire message; UIKit will take care
+							//  of line breaks and truncation.
+							preview.Text = msg.Text;
 	
-								// Setup the date label to automatically update
-								if (lastTimeStampSubscription != null) {
-									disposables.Remove (lastTimeStampSubscription);
-									lastTimeStampSubscription.Dispose ();
-								}
-								disposables.Add (
-									lastTimeStampSubscription = msg.TimeStamp.ObservePrettyString ().Subscribe (str => date.Text = str)
-								);
-							})
-					);
+							// Setup the date label to automatically update
+							timeStampSubscription?.Dispose ();
+							timeStampSubscription = msg.TimeStamp.ObservePrettyString ().Subscribe (str => date.Text = str);
+						});
 				}
 			}
 		}
